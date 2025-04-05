92c2efae4322ee88b167f814afa8b663acacea8d
Edwin <edwinreji75@gmail.com>
2025-04-05 22:52:23 +0000

    RLHF and reward model finalized. LoRA options added.

--- RLHF/utils.py
+++ RLHF/utils.py
@@ -5,4 +5,5 @@ def load_config(config_path):
     with open(config_path, "r") as f:
         config = yaml.safe_load(f)
     config["training"]["reward_model"]["learning_rate"] = float(config["training"]["reward_model"]["learning_rate"])
+    config["training"]["rlhf"]["learning_rate"] = float(config["training"]["rlhf"]["learning_rate"])
     return config
